8ed90ff0ce96afba12eeed9be701eaffbea10145
Diego Prieto <diego.prieto.dev@gmail.com>
2023-05-15 12:26:50 +0200

    AppStyled

---
+++ src/AppStyled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledMain = styled.main`
+  margin-top: ${({ isLoggedIn }) => (isLoggedIn ? "0" : "0")};
+  margin-left: ${({ sidebarVisible }) => (sidebarVisible ? "350px" : "30px")};
+  padding: ${({ isLoggedIn }) => (isLoggedIn ? "0" : "0")};
+  transition: all 0.4s;
+  width: ${({ sidebarVisible, isLoggedIn }) =>
+    isLoggedIn && sidebarVisible ? "calc(100% - 300px)" : "100%"};
+  background-color: #171717;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: ${({ isLoggedIn }) => (isLoggedIn ? "auto" : "hidden")};
+  margin: auto;
+  margin-left: ${({ sidebarVisible }) => (sidebarVisible ? "350px" : "auto")};
+`;
