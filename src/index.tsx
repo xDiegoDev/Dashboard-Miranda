06c1cf138fc3c9c9b8553dca934750b84c51670c
Diego Prieto <diego.prieto.dev@gmail.com>
2023-05-18 15:07:13 +0200

    Converted index.js to TS, and made changes to the tsconfig file

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import WrappedApp from "./App";
+import WrappedApp from "./App.tsx";
 import { store } from "./store/store.tsx";
 import { Provider } from "react-redux";
 
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+
+if (!rootElement) throw new Error("No root element found");
+
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <Provider store={store}>
     <AuthProvider>
